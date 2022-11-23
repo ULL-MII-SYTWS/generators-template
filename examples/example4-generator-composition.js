91ea0b83126174038b55197d196fc46530133595
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-23 12:03:45 +0000

    example4-generator-composition.js

--- examples/example4-generator-composition.js
+++ examples/example4-generator-composition.js
@@ -1,1 +1,28 @@
-// leave here the fourth example
+// The yield* directive delegates the execution to another generator.
+// This term means that yield* gen iterates over the generator gen
+// and transparently forwards its yields outside.
+// As if the values were yielded by the outer generator.
+
+function * mygen (first, last) {
+  for (let i = first; i <= last; i++) yield i
+}
+
+function * composed () {
+  yield * mygen('0'.charCodeAt(0), '9'.charCodeAt(0))
+  yield * mygen('A'.charCodeAt(0), 'Z'.charCodeAt(0))
+  yield * mygen('a'.charCodeAt(0), 'z'.charCodeAt(0))
+}
+
+const res = [...composed()]
+  .map(code => String.fromCharCode(code))
+  .join('')
+console.log(res)
+// 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+
+let str = ''
+
+for (const code of composed()) {
+  str += String.fromCharCode(code)
+}
+
+console.log(str)
