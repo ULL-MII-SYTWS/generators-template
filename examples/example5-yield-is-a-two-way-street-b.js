920475fb150760e24c4a6979e0395aa9c91ffe0e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-23 12:24:13 +0000

    example5-yield-is-a-two-way-street-b.js

--- examples/example5-yield-is-a-two-way-street-b.js
+++ examples/example5-yield-is-a-two-way-street-b.js
@@ -1,11 +1,11 @@
 // Section a = yield exp submits exp and receives the b of the next g.next(b)
 // What is the output?
 function * gen () {
-  returnedFromYield = yield 'foo' 
-  yield returnedFromYield 
+  const returnedFromYield = yield 'foo'
+  yield returnedFromYield
 }
 
-const g = gen();
+const g = gen()
 
-console.log(g.next(1)) 
+console.log(g.next(1))
 console.log(g.next(2))
