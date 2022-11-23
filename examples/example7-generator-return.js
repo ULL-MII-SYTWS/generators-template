d6850e398eaafd097bb91b4fbed53d6c16568978
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-23 12:44:19 +0000

    example7-generator-return.js

--- examples/example7-generator-return.js
+++ examples/example7-generator-return.js
@@ -1,1 +1,18 @@
-// leave here the seventh example
+// What is the output?
+function * foo () {
+  yield 123
+}
+
+function * bar () {
+  return yield 123
+}
+
+const f = foo()
+const b = bar()
+
+console.log(
+  f.next(),
+  f.next(),
+  b.next(),
+  b.next()
+)
